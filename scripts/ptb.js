2c9f7648f6a3dd844bc0c831a758bc1fb5df20e2
ecorey <bengstrom32@gmail.com>
2024-03-12 11:43:44 -0400

    ptb

--- scripts/ptb.js
+++ scripts/ptb.js
@@ -1,7 +1,7 @@
 import { SuiClient, getFullnodeUrl } from '@mysten/sui.js/client';
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 import { MIST_PER_SUI } from '@mysten/sui.js/utils';
-import { JsonRpcProvider, testnetConnection } from '@mysten/sui.js';
+// import { JsonRpcProvider, testnetConnection } from '@mysten/sui.js';
 
 
 
@@ -16,13 +16,19 @@ const ADDRESS_User = '0x6060640454e670a0efb91c896a7ee4f3d5781c9b489f9962640873d1
 const PACKAGE_ID = '0xaafa4058de49a7fb79d450c61e33ee03033c7c21634b24b73e0bf2a021798725'
 
 
+// path to an event that is emitted in the init function of the kiosk_practice contract
+const MoveEventType = '{PACKAGE_ID}::kiosk_practice::InitEvent';
 
-const MoveEventType = '{PACKAGE_ID}>::kiosk_practice::<METHOD_NAME>';
+
+// RPC for testnet
+// const provider = new JsonRpcProvider(testnetConnection);
 
 
 const balance = (balance) => {
 	return Number.parseInt(balance.totalBalance) / Number(MIST_PER_SUI);
 };
+
+
 
 const acctBalance = await suiClient.getBalance({
 	owner: ADDRESS_User,
@@ -36,13 +42,26 @@ const [coin] = txb.splitCoins(txb.gas, [10000000]);
 
 
 
-// subscrbe to events for the package
-let unsubscribe = await provider.subscribeEvent({
-    filter: { PACKAGE_ID },
-    onMessage: (event) => {
-        console.log("subscribeEvent", JSON.stringify(event, null, 2))
-    }
-});
+// console.log(
+//     await provider.getObject({
+//         id: Package,
+//         options: { showPreviousTransaction: true },
+//     }),
+// );
+
+
+
+
+// // subscribe to events for the package
+// let unsubscribe = await provider.subscribeEvent({
+//     filter: { PACKAGE_ID },
+//     onMessage: (event) => {
+//         console.log("subscribeEvent", JSON.stringify(event, null, 2))
+//     }
+// });
+
+
+
 
 // // call the set_predict_epoch function in the kiosk_practice contract
 // const [kiosk, kiosk_owner_cap] = txb.moveCall({
@@ -52,11 +71,12 @@ let unsubscribe = await provider.subscribeEvent({
 
 
 
+
 // call the set_predict_epoch function in the kiosk_practice contract
-const predict_epoch = txb.moveCall({
-    target: '0xaafa4058de49a7fb79d450c61e33ee03033c7c21634b24b73e0bf2a021798725::kiosk_practice::set_predict_epoch',
-    arguments: [ 2, 555 ],
-});
+// const predict_epoch = txb.moveCall({
+//     target: '0xaafa4058de49a7fb79d450c61e33ee03033c7c21634b24b73e0bf2a021798725::kiosk_practice::set_predict_epoch',
+//     arguments: [ 2, 555 ],
+// });
 
 
 
