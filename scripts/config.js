52bb392dab3bc0053980cd4886ed51eb7622426f
ecorey <bengstrom32@gmail.com>
2024-03-16 13:11:21 -0400

    keypair script

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,7 @@
 
 
+export const CLOCK = "0x6";
+
 export const PACKAGE = "0xaafa4058de49a7fb79d450c61e33ee03033c7c21634b24b73e0bf2a021798725";
 
 export const devWalletPublicKeyRef = "0xb3d4cb714181fec39c22d820c963da9cfac970d3ab77c464b0dea06ce673c3e5";
@@ -7,3 +9,5 @@ export const devWalletPublicKeyRef = "0xb3d4cb714181fec39c22d820c963da9cfac970d3
 export const walletOneRef = "0x6dc3f9438f890ff4031f6a2151b3ca02aebf9e1522f0014a956dd8ef067e3b01";
 
 export const walletTwoRef = "0xe5f5e09892328ff278b473f485cbf85cef8a9958112023c84126aec3d32b8114";
+
+export const walletThreeRef = "0x07095af51002db0e9be284b8dab97263f77fec2a1be68cd42b7dd2358a6eccdd"
